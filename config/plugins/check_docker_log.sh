58b213a0cc450b3408f4963332f29b9e7a52903c
eric.chang <eric.chang.1015@gmail.com>
2017-06-01 10:20:25 +0000

    add grs log monitor

---
+++ config/plugins/check_docker_log.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+container=""
+
+if [ $# -eq 0 ]
+then
+    echo -e "Arguments are missing!  Run $0 docker_name (login N mins) "
+    echo -e "Eg:  $0  web_srv 10m"
+    echo -e "Eg:  $0  web_srv"
+    exit 1
+fi
+
+container=$1
+
+if [ $# -eq 2 ]; then
+    args="--since $2"
+fi
+
+docker logs $container $args 2>&1 | grep -E "error|Error|ERROR| at" --color
+
+if [ $? == 0 ]; then
+    exit 2
+fi
+ 
+docker logs $container $args | grep -E "WARN "
+
+if [ $? == 0 ]; then
+    exit 1
+fi
